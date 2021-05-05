db65e7cafb6dc45d50905141d592df34954edf3b
Dhruti Mistry <dhruti.g17@gmail.com>
2021-05-05 15:37:49 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 import Title from './components/Title';
 import UploadForm from './components/UploadForm';
 import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
         <Title/>
         <UploadForm/>
-        <ImageGrid/>
+        <ImageGrid setSelectedImg={setSelectedImg}/>
+        { selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+
+        )}
     </div>
   )
 }
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,21 +1,22 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
-//import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ({ file, setFile }) => {
   const { progress, url } = useStorage(file);
-  console.log(progress,url);
 
   useEffect(() => {
-      if(url){
-          setFile(null);
-      }
-      
-  }, [url,setFile])
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
 
-    return (
-        <div className='progress-bar' style={{width:progress+'%'}}>progress</div>
-    )
-}
+  return (
+    <motion.div className="progress-bar"
+      initial={{ width: 0 }}
+      animate={{ width: progress + '%' }}
+    ></motion.div>
+  );
+} 
 
 export default ProgressBar;
